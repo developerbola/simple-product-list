f396ec9443ef5ffa119a9e19966416a00440e5a2
developerbola <developerbola08@gmail.com>
2026-03-26 22:54:44 +0500

    better formatDate function

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -33,7 +33,33 @@ import {
   updateProduct,
 } from "@/lib/localStorage";
 
-const formatDate = (date: string) => new Date(date).toLocaleString("en-US");
+const formatDate = (dateStr: string) => {
+  const date = new Date(dateStr);
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  return `${day} ${month}, ${year} ${hours}:${minutes}:${seconds}`;
+};
 
 const Products = () => {
   const [products, setProducts] = useState<any[]>([]);
